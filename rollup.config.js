5fa725a37134dd3a04dfbb51d2a5265fce1f7472
Shane Holloway <shane.holloway@ieee.org>
2018-09-13 17:55:31 -0600

    Completed integration of data-live attributes and hash-n-gzip sourcing

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
+const fs = require('fs')
 import rpi_resolve from 'rollup-plugin-node-resolve'
-//import rpi_hash_n_gzip from 'rollup-plugin-hash-n-gzip'
-import rpi_sse_build_events from 'rollup-plugin-web-build-events/cjs/sse'
+import rpi_hash_n_gzip from 'rollup-plugin-hash-n-gzip'
+import rpi_sse_build_events from 'rollup-plugin-web-build-events'
+import { datalive_renderHTMLTempalteFile } from 'rollup-plugin-web-build-events/cjs/server/dataLive'
 import rpi_jsy from 'rollup-plugin-jsy-lite'
+
+console.log()
+console.log('ROLLUP.CONFIG', new Date)
+console.log()
 
 const configs = []
 export default configs
 
+const pi_events = rpi_sse_build_events({onBuildUpdate})
+
 const sourcemap = true
+const watch = {clearScreen: false}
 const base_plugins = [
   rpi_resolve({modulesOnly:true}),
-  rpi_sse_build_events(),
+  rpi_hash_n_gzip({onBuildUpdate: pi_events}),
+  pi_events,
 ]
 const external = []
 
@@ -18,17 +28,21 @@ const add_jsy = name => {
   configs.push({
     input: `code/${name}.jsy`,
     output: { file: `public/umd/${name}.js`, format: 'umd', name, exports:'named', sourcemap },
-    plugins, external }) }
+    plugins, external, watch }) }
 
 const add_vanilla = name => {
   const plugins = [].concat(base_plugins)
   configs.push({
     input: `code/${name}.js`,
     output: { file: `public/umd/${name}.js`, format: 'umd', name, exports:'named', sourcemap },
-    plugins, external }) }
+    plugins, external, watch }) }
 
 
 add_jsy('body')
 add_jsy('head')
 add_jsy('console_notes')
 add_jsy('sse_reload')
+
+function onBuildUpdate({mapping}) {
+  datalive_renderHTMLTempalteFile({mapping, input: './template/index.html', output: './public/index.html'})
+}
