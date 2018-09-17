e4841ad3f0067588b96e4cbd414a74bb4bfd3ab7
Shane Holloway <shane.holloway@ieee.org>
2018-09-17 14:45:44 -0600

    Showcase project for rollup-plugin-web-build-events and rollup-plugin-hash-n-gzip

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 import rpi_resolve from 'rollup-plugin-node-resolve'
+import rpi_replace from 'rollup-plugin-replace'
+import rpi_strip_code from 'rollup-plugin-strip-code'
+
 import rpi_hash_n_gzip from 'rollup-plugin-hash-n-gzip'
 import rpi_sse_build_events from 'rollup-plugin-web-build-events'
 import { datalive_renderHTMLTempalteFile } from 'rollup-plugin-web-build-events/cjs/server/dataLive'
 import rpi_jsy from 'rollup-plugin-jsy-lite'
 
-console.log()
-console.log('ROLLUP.CONFIG', new Date)
-console.log()
 
 const configs = []
 export default configs
@@ -16,33 +16,55 @@ const pi_events = rpi_sse_build_events({onBuildUpdate})
 
 const sourcemap = true
 const watch = {clearScreen: false}
-const base_plugins = [
+
+const is_watch = -1 !== process.argv.indexOf('-w') || -1 !== process.argv.indexOf('--watch')
+const is_production = (/production/i).test(process.env.NODE_ENV)
+
+const plugins = [
+  rpi_replace({
+    include: 'code/*',
+    'process.env.NODE_ENV': JSON.stringify(is_production ? 'production' : 'development'),
+  }),
+
+  is_watch ? null : rpi_strip_code({
+    include: 'code/*',
+    start_comment: `BEGIN WATCH MODE`,
+    end_comment: `END WATCH MODE`,
+  }),
+
+  rpi_strip_code({
+    include: 'code/*',
+    start_comment: `BEGIN ${is_production ? 'DEV' : 'PROD'} ONLY`,
+    end_comment: `END ${is_production ? 'DEV' : 'PROD'} ONLY`,
+  }),
+
+  rpi_jsy(),
   rpi_resolve({modulesOnly:true}),
   rpi_hash_n_gzip({onBuildUpdate: pi_events}),
   pi_events,
-]
-const external = []
 
-const add_jsy = name => {
-  const plugins = [rpi_jsy()].concat(base_plugins)
+].filter(Boolean)
+
+
+add_jsy_script('head')
+add_jsy_module('body')
+add_jsy_module('console_notes')
+
+function onBuildUpdate({mapping}) {
+  datalive_renderHTMLTempalteFile(mapping, {input: './template/index.html', output: './public/index.html'})
+  fs.writeFile('./public/dynamic.json', JSON.stringify(mapping, null, 2), err=>err && console.error(err))
+}
+
+
+function add_jsy_module(name) {
+  configs.push({
+    input: `code/${name}.jsy`,
+    output: { dir: `public/esm/`, format: 'esm', sourcemap },
+    plugins, watch, experimentalCodeSplitting: true }) }
+
+function add_jsy_script(name) {
   configs.push({
     input: `code/${name}.jsy`,
     output: { file: `public/umd/${name}.js`, format: 'umd', name, exports:'named', sourcemap },
-    plugins, external, watch }) }
+    plugins, watch }) }
 
-const add_vanilla = name => {
-  const plugins = [].concat(base_plugins)
-  configs.push({
-    input: `code/${name}.js`,
-    output: { file: `public/umd/${name}.js`, format: 'umd', name, exports:'named', sourcemap },
-    plugins, external, watch }) }
-
-
-add_jsy('body')
-add_jsy('head')
-add_jsy('console_notes')
-add_jsy('sse_reload')
-
-function onBuildUpdate({mapping}) {
-  datalive_renderHTMLTempalteFile({mapping, input: './template/index.html', output: './public/index.html'})
-}
